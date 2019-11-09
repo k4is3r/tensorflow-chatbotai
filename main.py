bb87a03f95e3f7dd6dd80891f5b345c989a6a744
Eduardo Imery <ejwimery@gmail.com>
2019-11-09 04:05:44 -0600

    getting all the words to implement the trainig

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ import json
 with open("intents.json") as file:
     data = json.load(file)
 
-print(data)
+words = []
+lables = []
+docs = []
 
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs.append(pattern)
+        
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
  
