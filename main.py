8ae270fb07dcf9e50229d7673963940310f7fe92
Eduardo Imery <ejwimery@gmail.com>
2019-11-09 13:02:36 -0600

    model test 1

--- main.py
+++ main.py
@@ -64,4 +64,8 @@ net = tflearn.input_data(shape=[None, len(training[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(output[0]) , activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
 
