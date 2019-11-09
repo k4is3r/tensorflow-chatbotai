062abd0fa97f48f964ecc6d098f91a8f2957415e
Eduardo Imery <ejwimery@gmail.com>
2019-11-09 15:12:28 -0600

    loading files of training, making a function to analize a text

--- main.py
+++ main.py
@@ -7,55 +7,61 @@ import tflearn
 import tensorflow as tf
 import random
 import json 
+import pickle
 
 
 with open("intents.json") as file:
     data = json.load(file)
 
-words = []
-labels = []
-docs_x = []
-docs_y = []
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
-        
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
- 
-words = [stemmer.stem(w.lower()) for w in words if w !="?"]
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+            
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+    
+    words = [stemmer.stem(w.lower()) for w in words if w !="?"]
 
-words = sorted(list(set(words)))
+    words = sorted(list(set(words)))
 
-labels = sorted(labels)
+    labels = sorted(labels)
 
-#creating the training test
-training = []
-output = []
+    #creating the training test
+    training = []
+    output = []
 
-out_empty = [0 for _ in range(len(labels))]
+    out_empty = [0 for _ in range(len(labels))]
 
-for x, doc in enumerate(docs_x):
-    bag = []
-    wrds = [stemmer.stem(w) for w in doc]
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
-    training.append(bag)
-    output.append(output_row)
-
-#change variables to numpy for traing
-training = numpy.array(training)
-output = numpy.array(output)
+    for x, doc in enumerate(docs_x):
+        bag = []
+        wrds = [stemmer.stem(w) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        training.append(bag)
+        output.append(output_row)
+    #change variables to numpy for traing
+    training = numpy.array(training)
+    output = numpy.array(output)
+    with open("data.pickle", "w") as f:
+        pickle.dump((words, lavels, training, output),f)
 
 #stating model to traing
 tf.reset_default_graph()
@@ -68,12 +74,26 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-model.fit(
-          training,
-          output,
-          n_epoch=1000,
-          batch_size=8,
-          show_metric=True
-         )
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(
+              training,
+              output,
+              n_epoch=1000,
+              batch_size=8,
+              show_metric=True
+             )
+    model.save("model.tflearn")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i].append(1)
+    return numpy.array(bag)
+
 
